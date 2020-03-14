dcd7513ed637f42ae970a8120e267006fa86b05d
Jonily <1242324179@qq.com>
2020-03-14 23:58:42 +0800

    课堂

--- src/biClass.java
+++ src/biClass.java
@@ -1,6 +1,26 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 public class biClass {
     public static void main(String[] args) {
+        int [] array =  {1,2,3};
+        int [] ret = mycopy(array);
+        System.out.println(Arrays.toString(ret));
+    }
+    public static int [] mycopy(int [] array){
+          int [] ret = new int[array.length];
+          int i = 0 ;
+          for (i = 0 ; i<array.length;i++){
+              ret[i] = array[i];
+          }
+          return ret;
+    }
+
+
+
+
+
+    /*
         System.out.println("请输入数字：");
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
@@ -16,3 +36,5 @@ public class biClass {
         }
     }
 }
+*/
+    }
