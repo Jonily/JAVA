a4dd2402b985f4464fc7e658a6cb074219d48b11
Jonily <1242324179@qq.com>
2020-03-18 02:35:27 +0800

    随机点名器

--- 20200318/src/callName.java
+++ 20200318/src/callName.java
@@ -9,7 +9,11 @@ public class callName {
     public static void main(String[] args) {
         System.out.println("------------随机点名器------------");
         String[] student =  new String[10];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("您的学生数量为：");
+        int i = sc.nextInt();
         boolean flag = true;
+        System.out.println();
         while(flag){
             System.out.println("请选择序号来进行下一步操作：");
             System.out.println("================================");
@@ -17,24 +21,21 @@ public class callName {
             System.out.println("      （2）浏览学生");
             System.out.println("      （3）随机点名");
             System.out.println("      （4）退   出");
-            Scanner sc = new Scanner(System.in);
             int n = sc.nextInt();
         switch (n){
             case 1:{
-                System.out.println("您想要添加学生的个数为：");
-                int i = sc.nextInt();
                 addstudent(student,i);
                 System.out.println();
                 break;
             }
             case 2:{
-                printstudent(student);
+                printstudent(student,i);
                 System.out.println();
                 break;
             }
             case 3:{
-             String name = randomstudent(student);
-                System.out.println("被点到名字的同学是："+name);
+             String name = randomstudent(student,i);
+                System.out.println("   被点到名字的同学是："+name);
                 break;
             }
             case 4:{
@@ -42,7 +43,7 @@ public class callName {
                 break;
             }
             default:{
-                System.out.println("输入错误，请重新输入！");
+                System.out.println("      输入错误，请重新输入！");
                 System.out.println();
                 break;
             }
@@ -53,19 +54,20 @@ public class callName {
         Scanner sc = new Scanner(System.in);
         int a = 0;
         for (a = 0; a<i; a++ ){
-            System.out.println("储存第"+(a+1)+"个姓名：");
+            System.out.println("  储存第"+(a+1)+"个姓名：");
                stu[a] = sc.next();
         }
     }
-    public static void printstudent(String[] stu){
-        for (String x: stu ) {
-            System.out.print(" "+ x +" ");
+    public static void printstudent(String[] stu,int i){
+        System.out.println("您的学生有：");
+        for (int a = 0; a<i; a++ ){
+            System.out.print(" "+ stu[a] +" ");
         }
         System.out.println();
     }
-    public static String randomstudent(String[] stu){
-        int a = new Random().nextInt(stu.length);
-        System.out.println("第"+(a+1)+"个同学被点到了！");
+    public static String randomstudent(String[] stu,int i){
+        int a = new Random().nextInt(i);
+        System.out.println("      第"+(a+1)+"个同学被点到了！");
         String name = stu[a];
         return name ;
     }
