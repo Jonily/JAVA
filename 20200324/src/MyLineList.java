8911de76c60c96a4d70e4bafca4d7f50bbf86ef3
Jonily <1242324179@qq.com>
2020-03-24 23:57:12 +0800

    单链表

--- 20200324/src/MyLineList.java
+++ 20200324/src/MyLineList.java
@@ -5,7 +5,25 @@
  */
 class Node{
     private int data;
-    private 
+    private Node next; //利用引用类型储存地址，因为已经定义Node所以可以直接引用
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
 }
 public class MyLineList {
 }
