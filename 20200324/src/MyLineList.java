ad3d9909d2bcafd1bc0a2cb5e979380b8ad54e61
Jonily <1242324179@qq.com>
2020-03-25 23:52:06 +0800

    单链表

--- 20200324/src/MyLineList.java
+++ 20200324/src/MyLineList.java
@@ -7,14 +7,10 @@ class Node{
     private int data;
     private Node next; //利用引用类型储存地址，因为已经定义Node所以可以直接引用
 
-    public int getData() {
-        return data;
-    }
-
-    public void setData(int data) {
+    public Node(int data){
         this.data = data;
+        this.next = null;
     }
-
 
     public Node getNext() {
         return next;
@@ -24,6 +20,138 @@ class Node{
         this.next = next;
     }
 
+
+    public int getData() {
+        return data;
+    }
+    public void setData(int data) {
+        this.data = data;
+    }
 }
-public class MyLineList {
+//组成单链表
+ public class MyLineList {
+
+    public Node head; //定义一个头节点
+    public MyLineList(){
+        this.head = null;       //设置头节点为空，也可以不设置
+    }
+    //打印单链表
+    public void disPlay(){
+        Node cur = this.head;
+        while(cur!=null){
+            System.out.print(cur.getData()+" ");
+            cur = cur.getNext();
+        }
+    }
+    //头插法
+    public void addFirst(int data){
+        Node node = new Node(data);
+        if(this.head == null){
+            //单链表中无节点
+            this.head = node;
+            return;
+        }
+
+        node.setNext(this.head);
+        this.head = node;
+
+    }
+    //尾插法
+    public void addLast(int data){
+        Node node = new Node(data);
+        if(this.head == null){
+            //单链表中无节点
+            this.head = node;
+            return;
+        }
+        Node cur = this.head;
+        while(cur.getNext()!=null){
+            cur = cur.getNext();
+        }
+        //此时cur指向最后一个节点
+        cur.setNext(node);
+    }
+    //在任意位置插入,第一个数据节点为0号下标
+    public Node findIndex(int index){//找到prev
+        int count = 0;
+        Node cur = this.head;
+        while (count < index-1){
+            cur.setNext(cur.getNext());
+            count++;
+        }
+        return cur;
+    }
+    public void addIndex(int index,int data){
+        if(index<0||index>size()){
+            throw new RuntimeException("输入位置不合法");
+        }else if(index == 0){
+            addFirst(data);
+        }else if(index == size()){
+            addLast(data);
+        }else {
+            Node node = new Node(data);
+            Node prev = findIndex(data);
+            node.setNext(prev.getNext());
+            prev.setNext(node);
+        }
+    }
+    public int size(){
+        int count = 0;
+        Node cur = this.head;
+        while (cur!=null){
+            count++;
+            cur = cur.getNext();
+        }
+        return count;
+    }
+    //查找是否包含关键字key是否在单链表当中
+    public boolean contains(int key){
+        Node cur = this.head;
+        while (cur!=null){
+            if(cur.getData() == key){
+                return true;
+            }
+            cur = cur.getNext();
+        }
+        return false;
+    }
+    //删除第一次出现关键字为key的节点
+    private Node findPrev(int key){
+        Node prev = this.head;
+        while(prev.getNext()!=null){
+            if(prev.getNext().getData() == key){
+                return prev;
+            }
+            prev = prev.getNext();
+        }
+        return null;
+    }
+    public void remove(int key){
+        if(this.head.getData() == key){
+            this.head = this.head.getNext();
+            return;
+        }
+        Node prev = findPrev(key);
+        if(prev == null){
+            System.out.println("没有这个节点！");
+            return;
+        }
+        Node vel = prev.getNext();
+        prev.setNext(vel.getNext());
+    }
+}
+class Demo{
+    public static void main(String[] args) {
+        MyLineList myLineList = new MyLineList();
+        myLineList.addLast(2);
+        myLineList.addLast(3);
+        myLineList.addLast(4);
+        myLineList.addFirst(5);
+        myLineList.addIndex(2,10);
+
+        System.out.print(myLineList.contains(10));
+        myLineList.remove(3);
+        myLineList.disPlay();
+
+    }
 }
