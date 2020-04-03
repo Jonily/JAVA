b98fd20f8bb4bda6b20e271b9c362e6b842d08d6
Jonily <1242324179@qq.com>
2020-04-03 23:17:10 +0800

    动物类

--- 20200402/src/com/bit/Test/Animals.java
+++ 20200402/src/com/bit/Test/Animals.java
@@ -1,12 +1,14 @@
 package com.bit.Test;
 
+import java.util.Date;
+
 public class Animals {
     public String name;
     public Animals (String name){
         this.name = name;
     }
     public void eat(String food){
-        System.out.println(this.name+"吃"+food);
+        System.out.println("Animals:"+this.name+"吃"+food);
     }
 }
 class Dog extends Animals {
@@ -14,10 +16,28 @@ class Dog extends Animals {
         //构造父类
         super(name);
     }
+    public void eat(String food){
+        System.out.println("Animals:"+this.name+"吃"+food);
+    }
+    public void run(){
+        System.out.println(this.name+"在跑");
+    }
 }
 class Test{
+    public static void func(Animals animals){
+        animals.eat("面包");
+
+    }
+    public Animals func1(){
+        Dog dog = new Dog("将军");
+        return dog;
+    }
     public static void main(String[] args) {
-        Dog dog = new Dog("小黑");
-        dog.eat("面包");
+       //向下转型
+        Animals animals = new Dog("小黑");
+        //animals.eat();
+        //父类引用了子类对象  向上转型
+        /*Animals animals = new Dog("小黑");
+        animals.eat("面包");*/
     }
 }
