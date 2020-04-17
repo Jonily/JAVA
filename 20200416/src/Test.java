e42a4e3c2f8060db5ba0a9e8f5b2b6a591062e93
Jonily <1242324179@qq.com>
2020-04-17 11:16:16 +0800

    测试

---
+++ 20200416/src/Test.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description:
+ * @Author HuangZhuoLin
+ * @Date 2020/4/16 18:04
+ */
+
+public class Test {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        List<Integer> ret = new ArrayList<>();
+        if(ret.isEmpty()){
+            return null;
+        }
+        List<Integer> Line = new ArrayList<>();
+
+
+
+
+    }
+}
