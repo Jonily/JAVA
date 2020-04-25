b4d456e4642e6bb066b7cf88545affcd99365bb7
Jonily <1242324179@qq.com>
2020-04-25 12:32:50 +0800

    根据先序和中序构造二叉树

---
+++ 二叉树面试题/src/根据先序和中序构造二叉树/TreeInterview.java
@@ -0,0 +1,17 @@
+package 根据先序和中序构造二叉树;
+
+/**
+ * @Description:
+ * @Author HuangZhuoLin
+ * @Date 2020/4/25 11:41
+ */
+
+public class TreeInterview {
+    //preorder这个数组和inorder相同
+    //用index记录访问到哪个节点
+    private int index = 0;
+    public 二叉树转换成排序的双向链表.TreeInterview.TreeNode buildTree(int[] preorder, int[] inorder){
+        
+
+    }
+}
