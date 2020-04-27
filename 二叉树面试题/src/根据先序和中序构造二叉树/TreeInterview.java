4c8f5004bcdcae1e108ee3ba5fae65e26f9a6981
Jonily <1242324179@qq.com>
2020-04-27 23:50:39 +0800

    二叉树

--- 二叉树面试题/src/根据先序和中序构造二叉树/TreeInterview.java
+++ 二叉树面试题/src/根据先序和中序构造二叉树/TreeInterview.java
@@ -16,12 +16,35 @@ public class TreeInterview {
         TreeNode(int x) {
             val = x;
         }
-    //preorder这个数组和inorder相同
-    //用index记录访问到哪个节点
+    }
+
     private int index = 0;
     public TreeNode buildTree(int[] preorder, int[] inorder){
         index = 0;
-        //借助buildTreeHelper方法进行递归
+        return buildTreeHelper(preorder,inorder,0,inorder.length);
     }
-
+        private TreeNode buildTreeHelper(int[] preorder, int[] inorder, int inorderLeft, int inorderRight) {
+        if(inorderLeft >= inorderRight){
+            return null;
+        }
+        if(index >= preorder.length){
+            return null;
+        }
+            TreeNode newNode = new TreeNode(preorder[index]);
+            int pos = find(inorder,inorderLeft,inorderRight,newNode.val);//在中序遍历中找到节点的下标
+            index++;//只++一次
+            newNode.left = buildTreeHelper(preorder,inorder,inorderLeft,pos);
+            newNode.right = buildTreeHelper(preorder,inorder,pos+1,inorderRight);
+            return newNode;
+    }
+    private int find(int[] inorder, int inorderLeft, int inorderRight, int val) {
+        for(int i = inorderLeft;i < inorderRight; i++){
+            if(inorder[i] == val){
+                return i;
+            }
+        }
+        return -1;
+    }
 }
+
+
