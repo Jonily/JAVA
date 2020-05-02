925df0ac3b4f6fcae50eb521d77a88dfdb71d1be
Jonily <1242324179@qq.com>
2020-05-02 22:53:10 +0800

    堆

--- 20200428/src/优先队列/Test.java
+++ 20200428/src/优先队列/Test.java
@@ -3,17 +3,10 @@ package 优先队列;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 public class Test {
-    static class MyComp implements Comparator<Integer>{
-        @Override
-        public int compare(Integer o1, Integer o2) {
-            //如果认为01优先级比o2高，先出o1 compare返回<0的整数
-            //如果认为02优先级比o1高，先出o2 compare返回>0的整数
-            //如果一样，返回0
-            return o2 - o1;
-        }
-    }
+
     public static void main(String[] args) {
-        PriorityQueue<Integer> queue = new PriorityQueue<>(new MyComp());
+        PriorityQueue<Integer> queue = new PriorityQueue<>((o1,o2) -> o2 -o1);
+
         queue.offer(9);
         queue.offer(5);
         queue.offer(2);
@@ -28,3 +21,23 @@ public class Test {
         }
     }
 }
+/*
+    PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return o2-o1;
+        }
+    });
+
+*/
+/*static class MyComp implements Comparator<Integer>{
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        //如果认为01优先级比o2高，先出o1 compare返回<0的整数
+        //如果认为02优先级比o1高，先出o2 compare返回>0的整数
+        //如果一样，返回0
+        return o2 - o1;
+    }
+}*/
+
+//PriorityQueue<Integer> queue = new PriorityQueue<>(new MyComp());
