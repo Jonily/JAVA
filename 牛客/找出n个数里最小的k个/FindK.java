c3850e56d4e2dcf3b9dd34879e02c31a80e19658
Jonily <1242324179@qq.com>
2020-06-08 23:45:45 +0800

    牛客

---
+++ 牛客/找出n个数里最小的k个/FindK.java
@@ -0,0 +1,47 @@
+package 找出n个数里最小的k个;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class FindK {
+  public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String n = in.nextLine();
+        String []a = null;
+        a = n.split(" ");
+        int k = 0;
+        int []num = new int[a.length];
+        for (int i = 0; i < a.length ; i++) {
+            num[i] = Integer.parseInt(a[i]);
+            k = num[a.length-1];
+        }
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        for (int i = 0; i < num.length-1; i++) {
+            priorityQueue.offer(num[i]);
+        }
+      for (int j = 0; j < k  ; j++) {
+          System.out.print(priorityQueue.poll() + " ");
+      }
+    }
+
+
+ /*   public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String n = in.nextLine();
+        String []a = null;
+        a = n.split(" ");
+        int k = 0;
+        int []num = new int[a.length];
+        for (int i = 0; i < a.length ; i++) {
+            num[i] = Integer.parseInt(a[i]);
+            k = num[a.length-1];
+        }
+        Arrays.sort(num,0,num.length-1);
+        for (int i = 0; i < k; i++) {
+            System.out.print(num[i] +" ");
+
+        }
+
+    }*/
+}
