284fcb25acf638ca78dee77693f4031defa15ad1
Jonily <1242324179@qq.com>
2020-06-16 23:20:09 +0800

    IO操作

---
+++ IO操作/src/基础操作/IODemo.java
@@ -0,0 +1,19 @@
+package 基础操作;
+
+import java.io.File;
+
+public class IODemo {
+    public static void main(String[] args) {
+        File file = new File("D:\\.hhh.txt");
+
+        System.out.println("文件是否存在："+ file.exists());
+        System.out.println("文件是否存在：");
+        System.out.println("文件是否存在：");
+        System.out.println("文件是否存在：");
+        System.out.println("文件是否存在：");
+        System.out.println("文件是否存在：");
+
+    }
+
+
+}
