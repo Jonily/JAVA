ad2f3ff0d84bfd12da2ae2bae54a94ae654389a7
Jonily <1242324179@qq.com>
2020-06-19 20:59:02 +0800

    IO操作

---
+++ IO操作/src/流操作/IODemo2.java
@@ -0,0 +1,51 @@
+package 流操作;
+
+import java.io.*;
+
+public class IODemo2 {
+    public static void main(String[] args) throws IOException {
+        copyFile("D:\\test_dir\\p1.JPEG","D:\\test_dir\\1\\p2.JPEG");//拷贝路径和目标路径
+        copyFile2("D:\\test_dir\\p1.JPEG","D:\\test_dir\\1\\p2.JPEG");//拷贝路径和目标路径
+
+
+    }
+    private static void copyFile(String srcPath,String destPath) throws IOException {
+        //先创建File的实例
+        //需要创建实例BufferedInputStream和 BufferedOutputStream方法内部特有了缓冲区对象
+        FileInputStream fileInputStream = new FileInputStream(srcPath);
+        FileOutputStream fileOutputStream = new FileOutputStream(destPath);
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+        byte[] buffer = new byte[1024];
+        int length = -1;
+        while ((length = bufferedInputStream.read(buffer))!= -1 ) {
+            bufferedOutputStream.write(buffer,0,length);
+        }
+        //四个流对象关闭
+        //调用bufferedInputStream.close时，会自动关闭内部的，FileInputStream和FileOutputStream
+        bufferedInputStream.close();
+        bufferedOutputStream.close();
+    }
+    private static void copyFile2(String srcPath,String destPath) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream(srcPath);
+        FileOutputStream fileOutputStream = new FileOutputStream(destPath);
+        try ( BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+              BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream)){
+                  int len = -1;
+                  byte[] buffer = new byte[1024];
+                  while(true){
+                      len = bufferedInputStream.read(buffer);
+                      if(len == -1){
+                          break;
+                      }else {
+                          bufferedOutputStream.write(buffer,0,len);
+
+                      }
+                  }
+
+              }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ IO操作/src/流操作/IODemo3.java
@@ -0,0 +1,45 @@
+package 流操作;
+
+import java.io.*;
+
+public class IODemo3 {
+    public static void main(String[] args) throws IOException {
+
+        //不使用和使用缓冲区的区别，在比较大的文件会体现得比较明显
+        teatNoBUffer("D:\\test_dir\\p1.JPEG","D:\\test_dir\\1\\p2.JPEG");
+        teatBuffer("D:\\test_dir\\p1.JPEG","D:\\test_dir\\1\\p2.JPEG");
+    }
+    private static void teatNoBUffer(String srcPath,String destPath){
+        //读的时候就是一个字节，一个字节读，不使用缓存区
+        long beg = System.currentTimeMillis();
+        try(FileInputStream fileInputStream = new FileInputStream(srcPath);
+        FileOutputStream fileOutputStream = new FileOutputStream(destPath)){
+            int ch = -1;
+            while ((ch = fileInputStream.read())!= -1) {
+            //啥都不干
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("no buffer"+(end - beg)+"ms");
+    }
+    private static void teatBuffer(String srcPath,String destPath) throws IOException {
+        long beg = System.currentTimeMillis();
+        try(BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(srcPath));
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(destPath))){
+            int ch = -1;
+            while ((ch = bufferedInputStream.read())!= -1){
+
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("have buffer"+(end - beg)+"ms");
+
+
+    }
+}
