ec2de1a2fbc70abf330bff17d945b894d2f6a797
Jonily <1242324179@qq.com>
2020-07-15 12:05:07 +0800

    Servlet

--- Servlet/src/main/java/ServletDemo/ServletDemo10.java
+++ Servlet/src/main/java/ServletDemo/ServletDemo10.java
@@ -13,15 +13,17 @@ import java.io.IOException;
 public class ServletDemo10 extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //收到图片，直接把图片保存到路径
+        //收到图片，直接把图片保存到路径 d:/javaResource
         //提前准备目录
         String basePath = "d:/javaResource/images/";
         Part image = req.getPart("image");
         //获取想要上传的文件名
         String path = basePath + image.getSubmittedFileName();
+        //image.getContentType() 得到图片的格式
         image.write(path);//图片想要保存的路径
 
 
         resp.setContentType("text/html; charset = utf-8");
+        resp.getWriter().write("图片上传成功！");
     }
 }
