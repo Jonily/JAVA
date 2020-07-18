67b97ee81564c6257187369095d217c9811c530d
Jonily <1242324179@qq.com>
2020-07-18 18:21:26 +0800

    Blog

--- Blog/src/main/java/model/ArticleDao.java
+++ Blog/src/main/java/model/ArticleDao.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ArticleDao {
@@ -20,7 +21,6 @@ public class ArticleDao {
             statement.setString(2,article.getContent());
             statement.setString(3,article.getUserId());
 
-
             //3、执行SQL语句
             int ret = statement.executeUpdate();
             if(ret != 1){
@@ -34,12 +34,12 @@ public class ArticleDao {
             //4、释放连接
             DBUtil.close(connection,statement,null);
         }
-
     }
 
 
     //2、查看文章列表（把所有文章信息都查出来，不查正文）
     public List<Article> selectAll(){
+        List<Article> articles = new ArrayList<>();
         //1、建立连接
         Connection connection = DBUtil.getConnection();
         //2、拼装SQL
@@ -59,14 +59,70 @@ public class ArticleDao {
                 article.setArticleId(resultSet.getInt("articleId"));
                 article.setTitle(resultSet.getString("title"));
                 article.setUserId(resultSet.getInt("userId"));
-                articles.add
-
+                articles.add(article);
             }
+
+            return articles;
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
+        return null;
     }
-    //3、查看指定文章详情
+    //3、查看指定文章详情(查看正文)
+    public Article selectById(int articleId){
+        //1、建立连接
+        Connection connection = DBUtil.getConnection();
+        //2、构造SQL
+        String sql = "select * from article where articleId = ? ";
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        try {
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1,articleId);
+            //3、执行SQL
+            resultSet = statement.executeQuery();
+            //4、遍历结果集合
+            //由于id是主键，不会重复，预期只有一条记录
+            if(resultSet.next()){
+                Article article = new Article();
+                article.setArticleId(resultSet.getInt("articleId"));
+                article.setTitle(resultSet.getString("title"));
+                article.setContent(resultSet.getString("content"));
+                article.setUserId(resultSet.getInt("userId"));
+                return article;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            //5、释放连接
+            DBUtil.close(connection,statement,resultSet);
+        }
+        return null;
+    }
 
     //4、删除指定文章（给定文章ID删除）
+    public void delete(int articleId){
+        //1、获取连接
+        Connection connection = DBUtil.getConnection();
+        //2、拼装SQL
+        String sql = "delete from article where article = ?";
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1,articleId);
+            //3、执行SQL
+            int ret = statement.executeUpdate();
+            if(ret != 1){
+                System.out.println("删除文章失败！");
+                return;
+            }
+            System.out.println("删除文章成功！");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            //4、释放SQL
+            DBUtil.close(connection,statement,null);
+        }
+    }
 }
--- Blog/src/main/java/model/UserDao.java
+++ Blog/src/main/java/model/UserDao.java
@@ -1,5 +1,7 @@
 package model;
 
+
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -10,7 +12,12 @@ public class UserDao {
     //把一个User 对象插入到数据库中
     void add(User user){
         //1、获取到数据库连接
-        Connection connection = DBUtil.getConnection();
+        //常见异常原因
+        //1、url写错（数据库的IP，端口号、数据库名、url格式错误）
+        //2、数据库服务器没有启动
+        //3、连其他主机的数据库，会被mysql安全策略阻拦
+        Connection connection = DBUtil.getConnection();//和数据库建立连接
+
         //2、拼装SQL语句
         String sql = "insert into user values (null, ?, ?)";
         PreparedStatement statement = null;
@@ -21,11 +28,19 @@ public class UserDao {
 
             //3、执行SQL语句
             int ret = statement.executeUpdate();
+            if(ret != 1){
+                System.out.println("插入新用户失败!");
+                return;
+            }
+            System.out.println("插入新用户成功！");
 
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            //4、释放数据库连接
+            DBUtil.close(connection,statement,null);
         }
+
     }
 
     //2、按照名字查找用户（登录）
@@ -36,18 +51,29 @@ public class UserDao {
         String sql = "select * from user where name = ?";
         PreparedStatement statement = null;
         ResultSet resultSet = null;
-
         try {
             statement = connection.prepareStatement(sql);
             statement.setString(1,name);
             //3、执行SQL
             resultSet = statement.executeQuery();
 
-            //4、遍历结果
+            //4、遍历结果(预期name在数据库中不能重复，只有一个)
+            if(resultSet.next()){
+                User user = new User();
+                user.setUseId(resultSet.getInt("userId"));
+                user.setName(resultSet.getString("name"));
+                user.setPassword(resultSet.getString("password"));
+                return user;
+            }
+
 
         } catch (SQLException throwables) {
             throwables.printStackTrace();
+        }finally {
+            //5、释放连接
+            DBUtil.close(connection,statement,resultSet);
         }
+        return null;
     }
 
 }
