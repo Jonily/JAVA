905312659c63541f28b44f36261a8268f97ba6b1
Jonily <1242324179@qq.com>
2020-07-25 23:56:27 +0800

    MusicDemo

--- MusicDemo/src/DAO/MusicDao.java
+++ MusicDemo/src/DAO/MusicDao.java
@@ -118,7 +118,8 @@ public class MusicDao {
     /**
      * 上传音乐可以直接传入一个Music对象
      * 先把文件给服务器
-     * 之后给数据库一下就是将文件放到数据库中
+     * 之后给数据库
+     * 下面就是将文件放到数据库中
      */
     public static int Insert(String title, String singer, String time, String url,
                       int userid) {
@@ -149,7 +150,101 @@ public class MusicDao {
         return 0;
     }
 
-    
+
+    /**
+     * 通过ID删除歌曲
+     */
+    public int deleteMusicById(int id){
+        Connection connection = null;
+        PreparedStatement statement = null;
+
+        try{
+            String sql = "delete from  music where id=?";
+            connection = DBUtils.getConnect();
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1,id);
+            int ret = statement.executeUpdate();
+
+            if(ret == 1){
+                //同时删除中间表中的数据
+                //1、看中间表中是否有数据，如有那就删除
+                if(findLoveMusicById(id)){
+                   int ret2 = removeLoveMusicDelete(id);
+                   if(ret2 == 1){
+                       return 1;
+                   }
+                }else {
+                    //如果没有找到，说明这首歌没有，被添加到喜欢的列表
+                    return 1;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            DBUtils.getClose(connection,statement,null);
+        }
+        return 0;
+    }
+
+    /**
+     * 查看中间表是否有该ID歌曲
+     */
+    private boolean findLoveMusicById(int id) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+
+        try{
+            String sql = "select * from lovemusic where music_id=?";
+            connection = DBUtils.getConnect();
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1,id);
+            resultSet = statement.executeQuery();
+
+            if(resultSet.next()){
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            DBUtils.getClose(connection,statement,resultSet);
+        }
+        return false;
+    }
+
+    /**
+     *删除服务器上的音乐时，同时在我喜欢的列表的数据库中进行删除
+     */
+    private int removeLoveMusicDelete(int musicId) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+
+        try{
+            String sql = "delete  from  lovemusic where music_id=?";
+            connection = DBUtils.getConnect();
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1,musicId);
+            int ret = statement.executeUpdate();
+            if(ret == 1){
+                return ret;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            DBUtils.getClose(connection,statement,null);
+        }
+        return 0;
+    }
+
+    /**
+     * 添加音乐到“喜欢”列表中
+     * 用户-》音乐
+     * 多对多
+     * 需要中间表
+     */
+    public boolean insertLoveMusic(int userId,int musicId){
+        
+    }
     public static void main(String[] args) {
        /* List<Music> musicList = findMusicByStr("南");
         System.out.println(musicList);*/
