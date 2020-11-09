76c592039403cd3607d66bcc15321de10c2f0b88
Jonily <1242324179@qq.com>
2020-11-09 23:18:58 +0800

    javaeeSysterm

---
+++ javaeeSysterm/luck-draw/src/main/java/huangzhuolin/config/web/RequestResponseBodyMethodProcessorWrapper.java
@@ -0,0 +1,29 @@
+package huangzhuolin.config.web;
+
+
+import org.springframework.core.MethodParameter;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+public class RequestResponseBodyMethodProcessorWrapper implements HandlerMethodReturnValueHandler {
+
+    private final HandlerMethodReturnValueHandler delegate;
+
+    public RequestResponseBodyMethodProcessorWrapper(HandlerMethodReturnValueHandler delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    public boolean supportsReturnType(MethodParameter returnType) {
+        return delegate.supportsReturnType(returnType);
+    }
+
+    @Override
+    public void handleReturnValue(Object returnValue, MethodParameter returnType, ModelAndViewContainer mavContainer, NativeWebRequest webRequest) throws Exception {
+      /*  if(!(returnValue instanceof ResponseResult)){
+            returnValue = ResponseResult.ok(returnValue);
+        }*/
+        delegate.handleReturnValue(returnValue, returnType, mavContainer, webRequest);
+    }
+}
