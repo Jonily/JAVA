6afe01b5ed74898523c171b70b249cbdb9307692
Jonily <1242324179@qq.com>
2020-11-18 22:14:57 +0800

    javaeeSysterm

---
+++ javaeeSysterm/luck-draw/src/test/java/huangzhuolin/mapper/SettingMapper.java
@@ -0,0 +1,9 @@
+package huangzhuolin.mapper;
+
+import huangzhuolin.base.BaseMapper;
+import huangzhuolin.model.Setting;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SettingMapper extends BaseMapper<Setting> {
+}
