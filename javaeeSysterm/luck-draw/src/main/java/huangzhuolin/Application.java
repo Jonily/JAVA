cdebcc1e5241e806b2f00e83b9e0d41d040010a9
Jonily <1242324179@qq.com>
2020-12-04 13:43:27 +0800

    javaeeSysterm

---
+++ javaeeSysterm/luck-draw/src/main/java/huangzhuolin/Application.java
@@ -0,0 +1,14 @@
+package huangzhuolin;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("huangzhuolin.mapper")
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
