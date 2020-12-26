6891a5305343812c33a113229ac6189d866e0270
黄卓琳 <61574030+Jonily@users.noreply.github.com>
2020-12-26 21:29:18 +0800

    Add files via upload

---
+++ Offer/src/镜像二叉树.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode mirrorTree(TreeNode root) {
+        if(root == null){
+            return null;
+        }
+        TreeNode node = null;
+        node = root.left;
+        root.left = root.right;
+        root.right = node;
+        if(root.left != null){
+            mirrorTree(root.left);
+        }
+        if(root.right != null){
+            mirrorTree(root.right);
+        }
+        return root;
+
+    }
+}
