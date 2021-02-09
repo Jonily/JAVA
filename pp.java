d1e88d7408203fc6f9fea8263af729f4f0651f86
黄卓琳 <61574030+Jonily@users.noreply.github.com>
2021-02-09 23:01:13 +0800

    Add files via upload

---
+++ pp.java
@@ -0,0 +1,39 @@
+public class Sort{
+	public static void Sort(int[] array){
+		SortHelper(array,0,array.length());
+	}
+
+	public static void SortHelper(int[] array,int left,int right){
+		if(left >= right){
+			return;
+		}
+		int index = find(array,left,right);
+		SortHelepr(array,left,index-1);
+		SortHelepr(array,index+1,right);
+
+
+	}
+
+	public static int find(int[] array,int left,int right){
+		int val = array[right];
+		int low = left;
+		int high = right;
+
+		while(low < high){
+
+			while(low < high && array[low] <= val){
+				low++;
+			}
+
+			while(low < high && array[high] >= val){
+				high--;
+			}
+			if(low < high){
+				swap(array,low,high);
+			}
+		}
+
+		swap(array,low,right);
+		return low;
+		}
+	}
